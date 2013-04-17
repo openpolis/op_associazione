5015f2479f458d7e010b5a8710cc98da12fc17e0
verganis <stefano.vergani.it@gmail.com>
2013-04-17 18:19:00 +0200

    5xmille:slider ok,images missing

--- assets/js/jquery.nivo.slider.js
+++ assets/js/jquery.nivo.slider.js
@@ -152,7 +152,9 @@
                     }
                     if(child.attr('data-thumb')) vars.controlNavEl.append('<a class="nivo-control" rel="'+ i +'"><img src="'+ child.attr('data-thumb') +'" alt="" /></a>');
                 } else {
-                    vars.controlNavEl.append('<a class="nivo-control" rel="'+ i +'">'+ (i + 1) +'</a>');
+//                    vars.controlNavEl.append('<a class="nivo-control" rel="'+ i +'">'+ (i + 1) +'</a>');
+                    vars.controlNavEl.append('<a class="nivo-control" rel="'+ i +'">&middot;</a>');
+
                 }
             }
 
