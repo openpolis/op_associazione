421fa112ee1d07bb2c7684c9201f1bcdd2a6d60b
Guglielmo <guglielmo.celata@gmail.com>
2013-04-19 07:35:47 +0200

    active option added to exportsubscribers management command

--- management/commands/exportsubscribers.py
+++ management/commands/exportsubscribers.py
@@ -4,19 +4,27 @@ from django.core.management.base import BaseCommand, CommandError
 from django.db.models import Count
 
 from op_associazione.models import Associate, Membership
-from op_associazione import notifications
 
 class Command(BaseCommand):
     help = 'Export all subscribers, ever, paying or not'
 
-    option_list = BaseCommand.option_list 
+    option_list = BaseCommand.option_list + (
+        make_option('--active',
+            action='store_true',
+            dest='active',
+            default=False,
+            help='Export only active subscribers'),
+        )
 
     def handle(self, *args, **options):
         """
         Extracts subscribers in CSV format
         """
 
-        subscribers = Associate.objects.all()
+        if options['active']:
+            subscribers = [m.associate for m in Membership.objects.filter(is_active=True)]
+	else:
+            subscribers = Associate.objects.all()
             
         print('nome;cognome;email;location')
         for s in subscribers:
